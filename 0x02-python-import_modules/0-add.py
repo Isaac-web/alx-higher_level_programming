1104bdaa733d1156d7a73e5522a75d4623c2a9eb
Isaac-web <kanytakiy@gmail.com>
2022-06-30 13:41:08 -0700

    Correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3 
+#!/usr/bin/ptyhon3
 
-if __name__ == "__name__"
+if __name__ == "__main__":
     from add_0 import add
 
     a = 1
     b = 2
-    result = add_0.add(a, b)
 
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b))
