ec7c706d4e00917bc71ec7348866f463a0612dea
Isaac Takiy <58862891+Isaac-web@users.noreply.github.com>
2022-06-30 21:33:08 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
 
 if __name__ == "__main__":
     import sys
-    arg_count = len(sys.argv) 
-    if(arg_count - 1) > 0:
-        print("{} argument:".format(arg_count - 1))
-        for i in range(1, arg_count):
-            print("{} : {}".format(i, sys.argv[i]))
-    else:
-        print("{} arguments.".format(arg_count - 1))
+    print_arg(sys.argv)
