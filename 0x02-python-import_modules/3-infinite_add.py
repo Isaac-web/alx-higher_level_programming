4c1ba42782aefd1e9644b8b28c8ee72e68df4b2b
Isaac Takiy <58862891+Isaac-web@users.noreply.github.com>
2022-06-30 22:12:33 +0000

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    if len(argv - 1)
+        for i in range(argv):
+            sum += int(argv[i])
+    print("{}".format(sum))
