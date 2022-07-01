50250d66567e31abeddab86f8f8260ea44c5eb1d
Isaac-web <kanytakiy@gmail.com>
2022-07-01 02:37:25 -0700

    5. Can you C me now?

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,4 @@
+def print_reversed_list_integer(my_list=[]):
+    list_len = len(my_list)
+    for i in range(list_len):
+        print("{}".format(my_list[list_len - (i + 1)]), end="\n")
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+def no_c(my_string):
+    chars = list(my_string)
+    for i in range(len(chars)):
+        if chars[i] == "c" or chars[i] == "C":
+            chars[i] = ""
+    return "".join(chars)
