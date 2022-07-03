3b3e5ed5eb67ce44646fa774388e5547d5223dbb
Isaac Takiy <58862891+Isaac-web@users.noreply.github.com>
2022-07-03 12:21:39 +0000

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,6 @@
 def divisible_by_2(my_list=[]):
     length = len(my_list)
     result = []
-    if not length:
-        return
     for index in range(length):
         if my_list[index]%2 == 0:
             result.append(True)
