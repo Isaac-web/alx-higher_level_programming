91f26ab1b4d5212b60bb737f56df7166039781c4
Isaac Takiy <58862891+Isaac-web@users.noreply.github.com>
2022-07-03 09:36:00 +0000

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-  if matrix is None:
-    return
-  for line in matrix:
-    for element in line:
-      print(" {}".format(element), end="")
-    print("")
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
