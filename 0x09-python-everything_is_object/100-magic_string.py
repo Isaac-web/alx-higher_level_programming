f83c449db5f09b4069eac77e51640cc9d5dcedd4
Isaac Takiy <kanytakiy@gmail.com>
2022-08-28 09:24:22 +0000

    Add 100-magic_string.py

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Magic string module"""
+
+count = 1
+def magic_string():
+    """ returns the string times the number of iterations  """
+    return "Best School" * count
+    count += 1
