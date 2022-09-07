653f6f12dae437db9d25fe8dbc64ce661430c9d9
Isaac Takiy <kanytakiy@gmail.com>
2022-09-07 05:24:04 +0000

    best score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- select the name, score from rows in second table where scores >= 10
-SELECT score, name FROM second_table WHERE score >= 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
