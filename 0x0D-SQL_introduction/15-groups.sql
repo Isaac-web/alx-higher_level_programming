a424826c14fcee87da6ae28f0951602b4311af4d
Isaac Takiy <kanytakiy@gmail.com>
2022-09-07 05:49:11 +0000

    group scores

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- group rows by scores
-SELECT score, COUNT (*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
