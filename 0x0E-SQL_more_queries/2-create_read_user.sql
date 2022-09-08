5525a56b3c07ac59c0bb8829dca1307b79d5ecfa
Isaac Takiy <kanytakiy@gmail.com>
2022-09-08 04:44:41 +0000

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
--- creates a database and a user and grants previleges on the
--- database to the user
-CREATE DATABASE 
-	IF NOT EXISTS `hbtn_0d_2`;
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
-	IF NOT EXISTS user_0d_2@localhost
-	IDENTIFIED BY 'user_0d_2_pwd';
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-	ON `hbtn_0d_2d`.*
-	TO user_0d_2@localhost;
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost';
