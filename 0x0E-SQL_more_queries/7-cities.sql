87ff088d0d731d14a5dc42cfd35a8779154c8fc6
Isaac Takiy <kanytakiy@gmail.com>
2022-09-08 05:17:52 +0000

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,8 @@ IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE
 IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	state_id INT FOREIGN KEY REFERENCE KEY(states.id) REFERENCES states(id),
-	name VARCHAR(256) NOT NULL
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) 
+	REFERENCES hbtn_0d_usa.states(id)
 );
