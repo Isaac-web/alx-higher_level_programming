ea2982c9854388a3fe085838777242850254976e
Isaac-web <kanytakiy@gmail.com>
2022-10-03 11:19:42 +0000

    7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+const numberOfPrints = parseInt(process.argv[2]);
+
+if (!numberOfPrints) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < numberOfPrints; i++) {
+    console.log("C is fun");
+  }
+}
