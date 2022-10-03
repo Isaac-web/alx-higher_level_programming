eb40f90d6dca58d72d22a5a3ca87d69cd39a52a2
Isaac-web <kanytakiy@gmail.com>
2022-10-03 11:14:30 +0000

    5-to_integer.js and 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,4 @@
+let firstNumber = parseInt(process.argv[2]);
+
+if (!firstNumber) console.log("Not a number");
+else console.log("My number is: " + firstNumber);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+const strings = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (string of strings) {
+  console.log(string);
+}
