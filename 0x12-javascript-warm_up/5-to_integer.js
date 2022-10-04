b6c7e24d6eca757184bfe510fb74f11a5a9dd874
Isaac-web <kanytakiy@gmail.com>
2022-10-04 08:39:59 +0000

    Modify 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const firstNumber = parseInt(process.argv[2]);
-if (!firstNumber) console.log('Not a number');
-else console.log('My number is: ' + firstNumber);
+if (!firstNumber) console.log("Not a number");
+else console.log("My number: " + firstNumber);
