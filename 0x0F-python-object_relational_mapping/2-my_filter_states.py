3c8b42716adac6fef4775af2f1956b569a82a5c0
Isaac-web <kanytakiy@gmail.com>
2022-10-10 22:53:39 +0000

    Add 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+"""
+This script prints the states from the database
+the marches the argument passed
+"""
+
+
+def configure_db():
+    """
+    configures the database connection and returns the db
+    connection object
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    return MySQLdb.connect(
+        host="localhost", port=3306, user=username,
+        passwd=password, db=database)
+
+
+if __name__ == "__main__":
+    db = configure_db()
+    cur = db.cursor()
+    pattern = sys.argv[4]
+
+    try:
+        cur.execute(
+            "SELECT * FROM states WHERE name = {} ORDER BY id ASC"
+            .format(pattern))
+    except MySQLdb.Error as err:
+        print("Something went wrong.")
+    finally:
+        cur.close()
+        db.close()
