0e7463b1fdc916bc36b9b0679ea1f9f96ac24420
Isaac Takiy <kanytakiy@gmail.com>
2022-11-14 09:08:01 +0000

    Modify 1-filter_state.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-
+"""
+This module fetches all states
+that start with the letter N
+"""
 import MySQLdb
 import sys
 
-'''
-This script lists all the states staring with N 
-in the states table
-'''
 
 def configure_db():
+    """Connects to the database"""
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    return MySQLdb.connect(host='localhost', port=3306,
-            user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+            user=username, passwd=password, host="localhost", db=database)
+    return db
+
 
 if __name__ == "__main__":
     db = configure_db()
     cur = db.cursor()
 
-    try:
-        cur.execute(
-                "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-        rows = cur.fetchall()
+    result = cur.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER_BY id")
 
-        for row in rows:
-            if row.name[0] == 'N':
-                print(row)
-    except MySQL.Error as err:
-        print("Error reading data")
-    finally:
-        cur.close()
-        db.close()
+    for state in result.fetch_all():
+        print(state, end="\n")
