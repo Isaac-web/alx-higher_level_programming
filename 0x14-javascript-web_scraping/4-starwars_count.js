965d07d7b113d4b483aab4780dcf6b2fa14f5088
Isaac Takiy <kanytakiy@gmail.com>
2022-11-16 05:28:15 +0000

    Modify 4-starwars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // A script that prints the number of movies where the character Wedge Antilles is present
+const request = require("request");
 
 const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
+const reqURL = args[2];
+
 request(reqURL, function (error, response, body) {
   if (error) {
     console.log('error:', error); // Print the error if one occurred
