f8d0c3f1a20a49ca3c22b9fabcfc63b1d0e29b8a
Isaac Takiy <kanytakiy@gmail.com>
2022-11-18 10:21:40 +0000

    Add 7-model_state_fetch_all.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains a States class that
+maps to a states table in the database
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+def configure_db(username, password, database):
+    """
+    configures a new db engine
+    """
+    str = "mysql+pymysql://{}:{}@localhost:3306/{}".format(
+            username, password, database)
+    return create_engine(str)
+
+
+engine = configure_db(argv[1], argv[2], argv[3])
+session = Session(bind=engine)
+
+states = session.query(State).order_by(State.id).all()
+for s in states:
+    print("{}: {}".format(s.id, s.name))
--- 0x0F-python-object_relational_mapping/test.py
+++
@@ -1,6 +0,0 @@
-#!usr/bin/python3
-"""
-This module contains a States class that
-maps to a states table in the database
-"""
-
