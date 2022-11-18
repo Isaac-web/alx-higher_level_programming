41ae17e87e2ce11f125486b1ca725ba7a48f64d3
Isaac Takiy <kanytakiy@gmail.com>
2022-11-18 12:18:07 +0000

    Add 12-model_state_update.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains a States class that
+maps to a states table in the database
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+def configure_db(username, password, database):
+    """configures a new db engine"""
+    str = "mysql://{}:{}@localhost:3306/{}".format(
+            username, password, database)
+    return create_engine(str)
+
+
+if __name__ == "__main__":
+    try:
+        engine = configure_db(argv[1], argv[2], argv[3])
+        session = Session(bind=engine)
+
+        state = session.query(State).filter(State.id == 2).first()
+        state.name = "New Mexico"
+        session.commit()
+    except Exception as e:
+        print(e)
