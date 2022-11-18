88c4ab4f76444de56c216a94dc6d111279a9cdd3
Isaac Takiy <kanytakiy@gmail.com>
2022-11-18 09:37:42 +0000

    Step 2

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,11 +7,3 @@ and inherits from the Base class
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-
-Base = declarative_base()
-class State(Base):
-    """
-    Defines a states class that maps to the states 
-    table in the mysql database
-    """
-    pass
