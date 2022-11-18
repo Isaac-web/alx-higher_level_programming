861eef9555ac5201fb8a781d755946890dd73618
Isaac Takiy <kanytakiy@gmail.com>
2022-11-18 11:11:38 +0000

    Add 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script fetches the first state
+from the database
+"""
+if __name__ == "__main__":
+    from model_state import State, Base
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sys import argv
+
+    def configure_db(username, password, database):
+        """configures a new db engine"""
+        str = "mysql+pymysql://{}:{}@localhost:3306/{}".format(
+                username, password, database)
+        return create_engine(str)
+
+    try:
+        engine = configure_db(argv[1], argv[2], argv[3])
+        session = Session(bind=engine)
+
+        state = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(state.id, state.name))
+    except Exception as e:
+        print(e)
