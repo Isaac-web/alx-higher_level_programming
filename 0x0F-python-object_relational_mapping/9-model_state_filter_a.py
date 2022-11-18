92d90e5cb0af0d222e31e277c12aa6b8c5c66d8a
Isaac Takiy <kanytakiy@gmail.com>
2022-11-18 11:27:35 +0000

    Add 9-model_state_filter_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contains a States class that
+maps to a states table in the database
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+def configure_db(username, password, database):
+    """configures a new db engine"""
+    str = "mysql://{}:{}@localhost:3306/{}".format(
+            username, password, database)
+    return create_engine(str)
+
+
+if __name__ == "__main__":
+    try:
+        engine = configure_db(argv[1], argv[2], argv[3])
+        session = Session(bind=engine)
+
+        states = session.query(State).filter(
+                State.name.like("%a%")).order_by(State.id).all()
+
+        for s in states:
+            print("{}: {}".format(s.id, s.name))
+    except Exception as e:
+        print(e)
+    
+
