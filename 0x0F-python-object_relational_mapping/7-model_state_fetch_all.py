96e1612651783597b8c82ef31efb666b81e1ae47
Isaac Takiy <kanytakiy@gmail.com>
2022-11-18 10:24:03 +0000

    Step 1

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,20 +7,3 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
 from sys import argv
-
-
-def configure_db(username, password, database):
-    """
-    configures a new db engine
-    """
-    str = "mysql+pymysql://{}:{}@localhost:3306/{}".format(
-            username, password, database)
-    return create_engine(str)
-
-
-engine = configure_db(argv[1], argv[2], argv[3])
-session = Session(bind=engine)
-
-states = session.query(State).order_by(State.id).all()
-for s in states:
-    print("{}: {}".format(s.id, s.name))
